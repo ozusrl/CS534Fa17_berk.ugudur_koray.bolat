df0494d3d6bd488bebbad61d7be635af76c412cc
Berk Üğüdür <berkugudur@hotmail.com>
2017-12-30 15:02:57 +0300

    Segments and cell symbols are now drawn by BoardView

---
+++ src/View/BoardView.java
@@ -0,0 +1,70 @@
+package View;
+
+import Model.Cell;
+import Model.Game;
+import Model.Segment;
+import Model.Symbol;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by berku on 30.12.2017.
+ */
+public class BoardView extends JPanel {
+    private Game game;
+    private Map<Symbol, Image> imageMap;
+    private Image seg_0;
+    private Image seg_1;
+    private Image seg_start;
+
+    public BoardView(Game game) throws IOException {
+        this.game = game;
+        this.imageMap = new HashMap<>();
+        setImages();
+    }
+
+    private void setImages() throws IOException {
+        seg_0 = ImageIO.read(new File("img/seg_02.png"));
+        seg_1 = ImageIO.read(new File("img/seg_12.png"));
+        seg_start = ImageIO.read(new File("img/seg_start2.png"));
+        Symbol[] symbols = Symbol.values();
+        for (Symbol symbol : symbols) {
+            imageMap.put(symbol, ImageIO.read(new File("img/symbols/" + symbol.toString() + ".png")));
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintSegments(g);
+        paintSymbols(g);
+
+    }
+
+    protected void paintSegments(Graphics g) {
+        int segmentSize = game.getBoard().getNumOfSegments();
+        g.drawImage(seg_start, 0, 0, 96, 288, this);
+        for (int i = 0; i < segmentSize; i++) {
+            int x = i * 96;
+            int y = ((((i % 2) + 1) % 2) * 288);
+            Image img = (i % 2 == 0) ? seg_0 : seg_1;
+            g.drawImage(img, x, y, 192, 288, this);
+        }
+    }
+
+    protected void paintSymbols(Graphics g) {
+        int cellSize = 48; //Magic Number
+        for (Segment s : game.getBoard().getSegments()) {
+            for (Cell c : s.getCells()) {
+                Image image = imageMap.get(c.getSymbol());
+                g.drawImage(image, c.getX() + 24, c.getY() + 10, cellSize, cellSize, this);
+            }
+        }
+    }
+}
--- src/View/GameView.java
+++ src/View/GameView.java
@@ -2,34 +2,24 @@ package View;
 
 import Model.*;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class GameView extends JPanel {
     private ArrayList<Color> colors;
-    private Map<Symbol, Image> imageMap;
-    private Image seg_0;
-    private Image seg_1;
-    private Image seg_start;
+    private BoardView boardView;
     private Game game;
     private int numOfCells;
-    private int cellSize;
 
     public GameView(Game game) throws IOException {
         this.game = game;
         this.numOfCells = game.getBoard().getNumOfCells();
-        this.cellSize = 48;
         this.setBackground(new Color(73, 204, 212));
         this.colors = new ArrayList<>();
-        this.imageMap = new HashMap<>();
+        this.boardView = new BoardView(game);
         setColors();
-        setImages();
     }
 
     private void setColors() {
@@ -41,38 +31,11 @@ public class GameView extends JPanel {
         colors.add(new Color(122, 100, 0));
     }
 
-    private void setImages() throws IOException {
-        seg_0 = ImageIO.read(new File("img/seg_02.png"));
-        seg_1 = ImageIO.read(new File("img/seg_12.png"));
-        seg_start = ImageIO.read(new File("img/seg_start2.png"));
-        Symbol[] symbols = Symbol.values();
-        for (Symbol symbol : symbols) {
-            imageMap.put(symbol, ImageIO.read(new File("img/symbols/" + symbol.toString() + ".png")));
-        }
-    }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int segmentSize = game.getBoard().getNumOfSegments();
-        g.drawImage(seg_start, 0, 0, 96, 288, this);
-        for (int i = 0; i < segmentSize; i++) {
-            int x = i * 96;
-            int y = ((((i % 2) + 1) % 2) * 288);
-            Image img = (i % 2 == 0) ? seg_0 : seg_1;
-            g.drawImage(img, x, y, 192, 288, this);
-        }
-        paintSymbols(g);
+        boardView.paintComponent(g);
         paintPirates(g);
-    }
-
-    private void paintSymbols(Graphics g) {
-        for (Segment s : game.getBoard().getSegments()) {
-            for (Cell c : s.getCells()) {
-                Image image = imageMap.get(c.getSymbol());
-                g.drawImage(image, c.getX() + 24, c.getY() + 10, cellSize, cellSize, this);
-            }
-        }
     }
 
     private void paintPirates(Graphics g) {
