73334c8c6550a2e3819b89beb275d6e515b53bcd
Berk Üğüdür <berkugudur@hotmail.com>
2018-01-13 18:33:10 +0300

    Minor changes

--- src/Network/Command.java
+++ src/Network/Command.java
@@ -8,6 +8,6 @@ import java.io.Serializable;
  * Created by berku on 12.1.2018.
  */
 public interface Command extends Serializable{
-    public abstract void executeOn(Game game);
+    void executeOn(Game game);
 }
 
--- src/Network/MoveForward.java
+++ src/Network/MoveForward.java
@@ -5,12 +5,10 @@ import Model.Game;
 import Model.Pirate;
 import Model.Player;
 
-/**
- * Created by berku on 12.1.2018.
- */
 public class MoveForward implements Command {
     private Pirate pirate;
     private Card card;
+
     public MoveForward(Pirate pirate, Card card) {
         this.pirate = pirate;
         this.card = card;
@@ -20,11 +18,9 @@ public class MoveForward implements Command {
     public void executeOn(Game game) {
         Player player = game.getPlayers().get(this.pirate.getPlayerIndex());
         Pirate pirate = player.getPirates().get(this.pirate.getIndex());
-        System.out.println("gelen kart id: " + this.card.getIndex());
         Card card = null;
-        for(Card c : player.getHand()){
-            System.out.println("karsilastirdigim kart id: " + c.getIndex());
-            if(c.getIndex() == this.card.getIndex())
+        for (Card c : player.getHand()) {
+            if (c.getIndex() == this.card.getIndex())
                 card = c;
         }
         game.moveForward(pirate, card);
